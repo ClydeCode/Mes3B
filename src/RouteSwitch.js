321bafb529a9fefde2172fdc2f52d155ce780679
Clyde <noneclyde@gmail.com>
2022-12-29 14:35:31 +0200

    Add new route

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -4,6 +4,7 @@ import News from './pages/News.js';
 import Class from './pages/Class.js';
 import About from './pages/About.js';
 import Admin from './pages/Admin.js';
+import AdminYearbook from "./pages/AdminYearbook.js";
 
 export default function RouteSwitch() {
     return (
@@ -16,6 +17,7 @@ export default function RouteSwitch() {
                 <Route path="/Apie" element={ <About /> }></Route>
                 <Route path="/Admin" element={ <Navigate to="/Admin/News"/> }></Route>
                 <Route path="/Admin/News" element={ <Admin /> }></Route>
+                <Route path="/Admin/Yearbook" element={ <AdminYearbook /> }></Route>
             </Routes>
         </BrowserRouter>
     )
