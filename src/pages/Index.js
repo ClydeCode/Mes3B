9013514da2255b0924fb4b7bf754f2d142205e6b
Clyde <noneclyde@gmail.com>
2023-01-01 15:28:53 +0200

    Add state of data array, fetchData function and cards rendering

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,11 +1,29 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import { Fragment } from "react";
+import {Fragment, useState, useEffect} from "react";
 import NewsCard from "../components/NewsCard";
 import '../styles/index.css';
 import '/node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 export default function Index() {
+    const [news, setNews] = useState([]);
+
+    const fetchData = () => {
+        return fetch(process.env.REACT_APP_WEBAPI + "news")
+            .then((response) => response.json())
+            .then((data) => {
+                setNews(data.splice(data.length - 6, data.length))
+            })
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    useEffect(() => {
+        console.log(news);
+    }, [news]);
+
     return (
         <Fragment>
             <Navbar />
@@ -13,12 +31,13 @@ export default function Index() {
                 <section>
                     <h4>Naujienos</h4>
                     <ul className="news-cards">
-                        <NewsCard />
-                        <NewsCard />
-                        <NewsCard />
-                        <NewsCard />
-                        <NewsCard />
-                        <NewsCard />
+                        {news.map(newsItem => {
+                            return <NewsCard
+                                    key={newsItem.id}
+                                    title={newsItem.title}
+                                    description={newsItem.description}
+                                    imageUrl={newsItem.image} />
+                        })}
                     </ul>
                 </section>
             </main>
