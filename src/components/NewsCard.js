1ca35e8b0682755438dae725320c786f6856008a
Clyde <noneclyde@gmail.com>
2023-01-02 13:32:20 +0200

    Add showShortDate function

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -2,17 +2,24 @@ import '../styles/newsCard.css';
 import { MdOutlineUpdate } from "react-icons/md";
 
 export default function NewsCard(props) {
+    const showShortDate = () => {
+        const date = props.createdDate;
+        return date.substring(0, date.length - 9)
+    }
+
     return (
         <li className='news-card'>
             <img src={props.imagePath} alt=''></img>
-            <p>
-                {props.description}
-                {props.description.length > 75 && "[...]"}
-            </p>
-            <div className='news-card-info'>
-                <div className='date'>
-                    <MdOutlineUpdate />
-                    {props.createdDate}
+            <div className='news-card-body'>
+                <p>
+                    {props.description}
+                    {props.description.length > 75 && "[...]"}
+                </p>
+                <div className='news-card-info'>
+                    <div className='date'>
+                        <MdOutlineUpdate />
+                        {showShortDate()}
+                    </div>
                 </div>
             </div>
         </li>
