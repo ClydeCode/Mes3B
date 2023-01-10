2b05b70b323d9df9123a059a2e4c4d2728683edf
Clyde <noneclyde@gmail.com>
2023-01-10 19:40:18 +0200

    Add DeleteAdminModal component

---
+++ src/components/DeleteAdminModal.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import axios, * as others from 'axios';
+
+export default function DeleteAdminModal(props) {
+    const [show, setShow] = useState(false);
+
+    const handleClick = () => setShow(!show);
+
+    const handleSubmitForm = (e) => {
+        e.preventDefault();
+
+        try {
+            axios.delete(process.env.REACT_APP_WEBAPI + `News/${props.data.id}`);
+
+            window.location.reload();
+        } catch (err) {
+            console.log(err);
+        }
+
+    }
+
+    return (
+        <>
+            <Button variant="danger" size="sm" onClick={handleClick}>
+                Delete
+            </Button>
+
+            <Modal show={show} onHide={handleClick} animation={false}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Delete Option</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Are you sure about delete?</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClick}>
+                        Close
+                    </Button>
+                    <Button variant="danger" onClick={handleSubmitForm}>
+                        Delete
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
